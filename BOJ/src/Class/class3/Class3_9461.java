456dc113204c180cf04d6c9e43fbef61c65f3604
hjju <hjju@fin2b.com>
2022-07-13 09:30:22 +0900

    2022_07_13

---
+++ BOJ/src/Class/class3/Class3_9461.java
@@ -0,0 +1,20 @@
+package Class.class3;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class Class3_9461 {
+
+  public static void main(String[] args) throws Exception {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    int t = Integer.parseInt(br.readLine());
+    while (t-- > 0) {
+      int n = Integer.parseInt(br.readLine());
+      ArrayList<Long> seq = new ArrayList<>(Arrays.asList(1L, 1L, 1L));
+      for (int i=4; i<n+1; i++) seq.add(seq.get(i-4) + seq.get(i-3));
+      System.out.println(seq.get(n-1));
+    }
+  }
+}
