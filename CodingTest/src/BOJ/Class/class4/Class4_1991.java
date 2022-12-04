0a8ef49563baed41183679ef603ee5f22c4d96a4
hjju <hjju@fin2b.com>
2022-12-04 16:51:30 +0900

    Class Rename Duplicated

--- CodingTest/src/BOJ/Class/class4/Class4_1991.java
+++ CodingTest/src/BOJ/Class/class4/Class4_1991.java
@@ -8,13 +8,13 @@ import java.util.StringTokenizer;
 public class Class4_1991 {
 
   static int n;
-  static Node head;
+  static Node1991 head;
 
   public static void main(String[] args) throws IOException {
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     n = Integer.parseInt(br.readLine());
 
-    head = new Node('A', null, null);
+    head = new Node1991('A', null, null);
 
     StringTokenizer st;
 
@@ -36,44 +36,44 @@ public class Class4_1991 {
     postorder(head);
   }
 
-  private static void insertNode(Node tmp, char root, char left, char right) {
+  private static void insertNode(Node1991 tmp, char root, char left, char right) {
     if (tmp.value == root) {
-      tmp.left = left == '.' ? null : new Node(left, null, null);
-      tmp.right = right == '.' ? null : new Node(right, null, null);
+      tmp.left = left == '.' ? null : new Node1991(left, null, null);
+      tmp.right = right == '.' ? null : new Node1991(right, null, null);
     } else {
       if (tmp.left != null) insertNode(tmp.left, root, left, right);
       if (tmp.right != null) insertNode(tmp.right, root, left, right);
     }
   }
 
-  private static void preorder(Node node) {
-    if (node == null) return;
-    System.out.print(node.value); // 루트
-    preorder(node.left); // 왼쪽
-    preorder(node.right); // 오른쪽
+  private static void preorder(Node1991 node1991) {
+    if (node1991 == null) return;
+    System.out.print(node1991.value); // 루트
+    preorder(node1991.left); // 왼쪽
+    preorder(node1991.right); // 오른쪽
   }
 
-  private static void inorder(Node node) {
-    if (node == null) return;
-    inorder(node.left);
-    System.out.print(node.value);
-    inorder(node.right);
+  private static void inorder(Node1991 node1991) {
+    if (node1991 == null) return;
+    inorder(node1991.left);
+    System.out.print(node1991.value);
+    inorder(node1991.right);
   }
 
-  private static void postorder(Node node) {
-    if (node == null) return;
-    postorder(node.left);
-    postorder(node.right);
-    System.out.print(node.value);
+  private static void postorder(Node1991 node1991) {
+    if (node1991 == null) return;
+    postorder(node1991.left);
+    postorder(node1991.right);
+    System.out.print(node1991.value);
   }
 }
 
-class Node {
+class Node1991 {
   char value;
-  Node left;
-  Node right;
+  Node1991 left;
+  Node1991 right;
 
-  public Node(char value, Node left, Node right) {
+  public Node1991(char value, Node1991 left, Node1991 right) {
     this.value = value;
     this.left = left;
     this.right = right;
